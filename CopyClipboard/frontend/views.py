e49370d377dc653dec8149cf0e531420337c42cb
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-03-08 15:42:03 -0800

    Completed impementation of React and setup

--- CopyClipboard/frontend/views.py
+++ CopyClipboard/frontend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#Renders the frontend/html file
+def index(request, *args, **kwargs):
+    return render(request,'frontend/index.html')
