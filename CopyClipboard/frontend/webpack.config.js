dea18ac74f4079c1f0d092635b8f94dd54c05c66
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-03-10 22:32:27 -0700

    Sucessfully set up react router

--- CopyClipboard/frontend/webpack.config.js
+++ CopyClipboard/frontend/webpack.config.js
@@ -1,36 +1,3 @@
-// const path = require("path");
-// const webpack = require("webpack");
-
-// module.exports = {
-//   entry: "./src/index.js",
-//   output: {
-//     path: path.resolve(__dirname, "./static/frontend"),
-//     filename: "[name].js",
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//         },
-//       },
-//     ],
-//   },
-//   optimization: {
-//     minimize: true,
-//   },
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       "process.env": {
-//         // This has effect on the react lib size
-//         NODE_ENV: JSON.stringify("production"),
-//       },
-//     }),
-//   ],
-// };
-
 const path = require("path");
 const webpack = require("webpack");
 
@@ -62,5 +29,11 @@ module.exports = (env, argv) => {
         "process.env.NODE_ENV": JSON.stringify(argv.mode),
       }),
     ],
+    devServer: {
+      contentBase: path.join(__dirname, "static/frontend"),
+      compress: true,
+      port: 9000,
+      historyApiFallback: true, // This is important for React Router to work with client-side routing
+    },
   };
 };
